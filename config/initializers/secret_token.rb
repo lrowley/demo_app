62b426565069f602f5e26bcc3a1ff428e14f16a4
lrowley <lelandrowley@gmail.com>
2012-03-29 15:30:10 -0600

    First Commit. Unicorns and Puppies.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '46217299a44ec6b798ce4a1c33cfae05f1dc0b1ae0e5ca76438352f97ae93483e53b1fc65bcbd6e05b882eb42a90994d8748f0ab603c00a73c6f0ee43a3bb2d3'
